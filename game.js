a6a376fdfe6a8569913cfbdf0dfd264169206ba7
Lukasz Tolwinski <lukasz.adam.tolwinski@gmail.com>
2021-08-09 19:40:24 +0200

    add publishResult

--- game.js
+++ game.js
@@ -41,6 +41,31 @@ function checkResult(player, ia) {
 
 }
 
+//publikacja wyniku
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "Wygrałeś! :)";
+        document.querySelector('[data-summary="who-win"]').style.color = "green";
+    } else if (result === "loss") {
+        document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+        document.querySelector('[data-summary="who-win"]').textContent = "Przegrałeś! :(";
+        document.querySelector('[data-summary="who-win"]').style.color = "red";
+    } else {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "Remis :|";
+        document.querySelector('[data-summary="who-win"]').style.color = "blue";
+    }
+
+
+}
+
+
 //funkcja sterująca
 function startGame() {
     if (!game.playerHand) return alert("Wybierz dłoń!");
@@ -48,6 +73,7 @@ function startGame() {
     game.aiHand = iaChoice();
     const gameResult = checkResult(game.playerHand, game.aiHand);
     console.log(gameResult);
+    publishResult(game.playerHand, game.aiHand, gameResult);
 }
 
 
